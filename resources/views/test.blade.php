a40f1a420deb7b9723f02faa82cccc1487f39a49
FAIZAN HAFEEZ <mfaizan.hafeez@arhamsoft.org>
2024-04-24 23:43:41 +0500

    first commit

---
+++ resources/views/test.blade.php
@@ -0,0 +1,108 @@
+@extends('layouts.app')
+
+@section('content')
+    <div class="container">
+        <h1 class="text-center text-success mt-5 mb-5"><b>Laravel 11 Datatables Yajra </b></h1>
+        <div class="card">
+            <div class="card-header">
+                <div class="row">
+                    <div class="col col-9"><b>USERS DATA</b></div>
+                    <div class="col col-3">
+                        <div id="daterange" class="float-end"
+                            style="background: #fff; cursor: pointer; padding: 5px 10px; border: 1px solid #ccc; width: 100%; text-align:center">
+                            <i class="fa fa-calendar"></i>&nbsp;
+                            <span></span>
+                            <i class="fa fa-caret-down"></i>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="card-body">
+                <table class="table table-bordered" id="daterange_table">
+                    <thead>
+                        <tr>
+                            <th>No</th>
+                            <th>Name</th>
+                            <th>Email</th>
+                            <th>Created On</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                    </tbody>
+                </table>
+            </div>
+        </div>
+    </div>
+    <!-- Include required libraries -->
+    <link rel="stylesheet" type="text/css" href="https://cdn.datatables.net/1.11.5/css/dataTables.bootstrap5.min.css">
+    <link rel="stylesheet" type="text/css" href="https://cdn.datatables.net/buttons/2.3.0/css/buttons.bootstrap5.min.css">
+    <script src="https://cdn.datatables.net/1.11.5/js/jquery.dataTables.min.js"></script>
+    <script src="https://cdn.datatables.net/1.11.5/js/dataTables.bootstrap5.min.js"></script>
+    <script src="https://cdn.datatables.net/buttons/2.3.0/js/dataTables.buttons.min.js"></script>
+    <script src="https://cdn.datatables.net/buttons/2.3.0/js/buttons.bootstrap5.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jszip/3.1.3/jszip.min.js"></script>
+    <script src="https://cdn.datatables.net/buttons/2.3.0/js/buttons.html5.min.js"></script>
+    <script src="https://cdn.datatables.net/buttons/2.3.0/js/buttons.print.min.js"></script>
+    <!-- Include moment.js for date range picker -->
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/moment.js/2.29.1/moment.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/moment.js/2.29.1/moment-with-locales.min.js"></script>
+    <!-- Include date range picker -->
+    <link rel="stylesheet" type="text/css" href="https://cdn.jsdelivr.net/npm/daterangepicker/daterangepicker.css" />
+    <script type="text/javascript" src="https://cdn.jsdelivr.net/npm/daterangepicker/daterangepicker.min.js"></script>
+    <script type="text/javascript">
+        $(function() {
+
+            var start_date = moment().subtract(1, 'M');
+
+            var end_date = moment();
+
+            $('#daterange span').html(start_date.format('MMMM D, YYYY') + ' - ' + end_date.format('MMMM D, YYYY'));
+
+            $('#daterange').daterangepicker({
+                startDate: start_date,
+                endDate: end_date
+            }, function(start_date, end_date) {
+                $('#daterange span').html(start_date.format('MMMM D, YYYY') + ' - ' + end_date.format(
+                    'MMMM D, YYYY'));
+
+                table.draw();
+            });
+
+            var table = $('#daterange_table').DataTable({
+                dom: 'Bfrtip',
+                buttons: [
+                    'copy', 'csv', 'excel', 'pdf', 'print'
+                ],
+                processing: true,
+                serverSide: true,
+                ajax: {
+                    url: "{{ route('users.index') }}",
+                    data: function(data) {
+                        data.from_date = $('#daterange').data('daterangepicker').startDate.format(
+                            'YYYY-MM-DD');
+                        data.to_date = $('#daterange').data('daterangepicker').endDate.format(
+                            'YYYY-MM-DD');
+                    }
+                },
+                columns: [{
+                        data: 'id',
+                        name: 'id'
+                    },
+                    {
+                        data: 'name',
+                        name: 'name'
+                    },
+                    {
+                        data: 'email',
+                        name: 'email'
+                    },
+                    {
+                        data: 'created_at',
+                        name: 'created_at'
+                    }
+                ]
+            });
+
+        });
+    </script>
+@endsection
---
+++ routes/web.php
@@ -0,0 +1,15 @@
+<?php
+
+use App\Http\Controllers\UserController;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Route;
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Auth::routes();
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+Route::get('users', [UserController::class, 'index'])->name('users.index');
+
